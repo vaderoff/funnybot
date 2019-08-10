067be57d92145332122418e0fa2e853728b58ab1
egor_parfenenko <offvader@gmail.com>
2019-08-10 17:01:53 +0300

    initial

---
+++ bot/bot.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher, executor, types
+import os
+import motor.motor_tornado
+import random
+
+bot = Bot(os.environ.get('BOT_TOKEN'))
+dp = Dispatcher(bot)
+
+db = motor.motor_tornado.MotorClient().funnybot
+
+
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def message_handler(message: types.Message):
+    words = message.text.split()
+    await db.words.insert_many([{'word': x} for x in words])
+
+    await db.message_counters.update_one(
+        {'chat_id': message.chat.id},
+        {
+            '$inc': {'count': 1},
+            '$set': {'chat_id': message.chat.id}
+        },
+        upsert=True)
+
+    counter = await db.message_counters.find_one({'chat_id': message.chat.id})
+    if counter.get('count') >= os.environ.get('COUNT_TO_RESET'):
+        await db.message_counters.update_one(
+            {'_id': counter.get('_id')},
+            {'$set': {'count': 0}}
+        )
+
+        words = list(await db.words.find())
+        random_words_count = random.choice(range(10))
+        random_words = random.choices(words, k=random_words_count)
+        sentence = ' '.join([x['word'] for x in random_words])
+
+        await message.reply(sentence)
---
+++ run.py
@@ -0,0 +1,4 @@
+from bot.bot import executor, dp
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
